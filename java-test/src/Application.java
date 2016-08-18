46eb33cdd160d4bd5bf766301ca427d3664fc2da
Chris <krzysiekzabicki@interia.pl>
2016-08-18 16:38:40 +0200

    used Scanner to get user's input

--- java-test/src/Application.java
+++ java-test/src/Application.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 /**
  * Created by chris on 18.08.16.
  */
 public class Application {
     public static void main(String[] args){
-        
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a line of text: ");
+        String line = input.nextLine();
+        System.out.println("Entered text is: " + line);
     }
 }
