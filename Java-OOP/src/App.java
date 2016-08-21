872ca796bee821f530d28456a937d8dd3c8d256f
Chris <krzysiekzabicki@interia.pl>
2016-08-21 23:57:41 +0200

    used StringBuilder and it's append() method

--- Java-OOP/src/App.java
+++ Java-OOP/src/App.java
@@ -8,5 +8,13 @@ public class App {
         info += "Hello everybody!";
 
         System.out.println(info);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("My name is Chris.");
+        sb.append(" ");
+        sb.append("Hello everybody!");
+
+        System.out.println(sb);
     }
 }
