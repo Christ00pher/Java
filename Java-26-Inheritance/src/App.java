57f1b8555c06b6e37f04e6aa322c2bde4a23538c
Chris <krzysiekzabicki@interia.pl>
2016-08-23 23:14:24 +0200

    Added 'Machine' class in seperated file 'Machine.java' and created 2 method in it

--- Java-26-Inheritance/src/App.java
+++ Java-26-Inheritance/src/App.java
@@ -3,6 +3,8 @@
  */
 public class App {
     public static void main(String[] args) {
-        
+        Machine mach1 = new Machine();
+        mach1.start();
+        mach1.stop();
     }
 }
---
+++ Java-26-Inheritance/src/Machine.java
@@ -0,0 +1,11 @@
+/**
+ * Created by chris on 23.08.16.
+ */
+public class Machine {
+    public void start() {
+        System.out.println("Machine started");
+    }
+    public void stop() {
+        System.out.println("Machine stopped");
+    }
+}
