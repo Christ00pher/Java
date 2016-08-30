de95f05c67d3fdb39f53d30e389d00f55bf642ef
Chris <krzysiekzabicki@interia.pl>
2016-08-30 21:07:01 +0200

    Added 'souts' to show maximum values of every numerical type

--- Java-32-Casting-Numerical-Values/src/App.java
+++ Java-32-Casting-Numerical-Values/src/App.java
@@ -3,6 +3,21 @@
  */
 public class App {
     public static void main(String[] args) {
-        
+
+        byte byteValue = 20;
+        short shortValue = 55;
+        int intValue = 888;
+        long longValue = 301232;
+
+        float floatValue = 555.3f;
+        double doubleValue = 29.6;
+
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
     }
 }
