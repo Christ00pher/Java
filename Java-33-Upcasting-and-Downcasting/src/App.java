b6c54e46f785ba590eaf9735dd70b14ca3cc02f5
Chris <krzysiekzabicki@interia.pl>
2016-09-01 23:38:24 +0200

    Added 2 classes and used polymorphism in main() function

--- Java-33-Upcasting-and-Downcasting/src/App.java
+++ Java-33-Upcasting-and-Downcasting/src/App.java
@@ -1,8 +1,28 @@
-/**
- * Created by chris on 01.09.16.
- */
+class Machine {
+    public void start() {
+        System.out.println("Machine started");
+    }
+}
+
+class Camera extends Machine {
+    public void start() {
+        System.out.println("Camera started");
+    }
+
+    public void snap() {
+        System.out.println("Photo taken");
+    }
+}
 public class App {
     public static void main(String[] args) {
-        
+
+        Machine machine1 = new Machine();
+        Camera camera1 = new Camera();
+
+        machine1.start();
+        camera1.start();
+        camera1.snap();
+
+        Machine machine2 = new Camera();
     }
 }
